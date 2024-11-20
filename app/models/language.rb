40674740edf52defeee86c4adf3a5f25cd547efc
Joel Armenta <armentamj@gmail.com>
2024-11-20 15:30:44 +0100

    Coded the appropriate has_many and such.

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,2 +1,3 @@
 class Language < ApplicationRecord
+  belongs_to :language_ability
 end
--- app/models/language_ability.rb
+++ app/models/language_ability.rb
@@ -1,4 +1,5 @@
 class LanguageAbility < ApplicationRecord
   belongs_to :doctor
   belongs_to :language
+  has_many :langauges
 end
